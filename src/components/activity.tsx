5205bb98a2b688e926f45084d6eed508dfc59cdb
Eduard Ramirez <edd.ramirez@proton.me>
2024-03-20 17:18:39 -0500

    Add IconPropsModel interface and update icon components

--- src/components/activity.tsx
+++ src/components/activity.tsx
@@ -1,27 +1,25 @@
-import { component$ } from "@builder.io/qwik";
+import { IconPropsModel } from "../models/icon-props.model";
 
-interface Props {
-  color?: string;
-  size?: string | number;
-}
-export const ActivityIcon = component$<Props>(
-  ({ size = "44", color = "#2c3e50" }) => {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="icon icon-tabler icon-tabler-activity"
-        width={size || "44"}
-        height={size || "44"}
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke={color || "#2c3e50"}
-        fill="none"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      >
-        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        <path d="M3 12h4l3 8l4 -16l3 8h4" />
-      </svg>
-    );
-  }
-);
+export const ActivityIcon = ({
+  size = 44,
+  stroke = 1.5,
+  color = "#2c3e50",
+}: IconPropsModel) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="icon icon-tabler icon-tabler-activity"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      stroke-width={stroke}
+      stroke={color}
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M3 12h4l3 8l4 -16l3 8h4" />
+    </svg>
+  );
+};
