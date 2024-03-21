30af21aa8e98d3e4e5b45bbd4640e0c0500ee8a1
CulmaOscar <oscarculma1999@gmail.com>
2024-03-20 21:07:11 -0500

    24 HORAS Y 2FA

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { TwelveHoursIcon } from "./components/twelve-hours";
 export { OneTwoThreeIcon } from "./components/one-two-three";
+export { TwentyFourHoursIcon } from "./components/twenty-four-hours";
+export { TwofaIcon } from "./components/twofa";
--- src/root.tsx
+++ src/root.tsx
@@ -1,5 +1,7 @@
 import { OneTwoThreeIcon } from "./components/one-two-three";
 import { TwelveHoursIcon } from "./components/twelve-hours";
+import { TwentyFourHoursIcon } from "./components/twenty-four-hours";
+import { TwofaIcon } from "./components/twofa";
 
 export default () => {
   return (
@@ -11,6 +13,8 @@ export default () => {
       <body>
         <TwelveHoursIcon />
         <OneTwoThreeIcon />
+        <TwentyFourHoursIcon />
+        <TwofaIcon />
       </body>
     </>
   );
