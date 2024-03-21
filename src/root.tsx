3bf42a0fe90f096e35064d38a304e23c580b6f63
edd.ramirez@proton.me <edd.ramirez@proton.me>
2024-03-21 13:30:15 -0500

    Update TablerIcon stroke and name

--- src/root.tsx
+++ src/root.tsx
@@ -8,7 +8,7 @@ export default () => {
         <title>Qwik Blank App</title>
       </head>
       <body>
-        <TablerIcon name="yoga" color="#c13030" size={150} stroke={1.8} />
+        <TablerIcon name="zzz-off" color="#c13030" size={150} stroke={1.5} />
       </body>
     </>
   );
