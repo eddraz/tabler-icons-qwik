9e4582645da75cea4a99a56f082a2dfcdd8897b2
CulmaOscar <oscarculma1999@gmail.com>
2024-03-20 23:34:23 -0500

    Registration 13 icons

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 export { TwelveHoursIcon } from "./components/twelve-hours";
 export { OneTwoThreeIcon } from "./components/one-two-three";
-<<<<<<< HEAD
 export { TwentyFourHoursIcon } from "./components/twenty-four-hours";
 export { TwofaIcon } from "./components/twofa";
-=======
 export { ActivityIcon } from "./components/activity";
 export { BoneIcon } from "./components/bone";
 export { CatIcon } from "./components/cat";
@@ -20,4 +18,4 @@ export { UserCircleIcon } from "./components/user-circle";
 export { ZoomScanIcon } from "./components/zoom-scan";
 export { ZzzIcon } from "./components/zzz";
 export { ZzzOffIcon } from "./components/zzz-off";
->>>>>>> 1e66107dc7ba306a3b7113829e51b4d3a8579be6
+export { ThreehundredAndSixtyViewIcon } from "./components/three-hundred-and-sixty-view";
--- src/root.tsx
+++ src/root.tsx
@@ -1,3 +1,11 @@
+import { ABIcon } from "./components/a-b";
+import { ABOffIcon } from "./components/a-b-off";
+import { ABTwoIcon } from "./components/a-b-two";
+import { AbacusIcon } from "./components/abacus";
+import { AbacusOffIcon } from "./components/abacus-off";
+import { AbcIcon } from "./components/abc";
+import { AccessPointIcon } from "./components/access-point";
+import { AccessPointOffIcon } from "./components/access-point-off";
 import { ActivityIcon } from "./components/activity";
 import { BoneIcon } from "./components/bone";
 import { CatIcon } from "./components/cat";
@@ -11,16 +19,18 @@ import { NewsIcon } from "./components/news";
 import { OneTwoThreeIcon } from "./components/one-two-three";
 import { ShoppingCartIcon } from "./components/shopping-cart";
 import { StethoscopeIcon } from "./components/stethoscope";
+import { ThreeDCubesPhereIcon } from "./components/three-d-cubes-phere";
+import { ThreeDCubesPhereOffIcon } from "./components/three-d-cubes-phere-off";
+import { ThreeDRotateIcon } from "./components/three-d-rotate";
+import { ThreeHundredAndSixtyIcon } from "./components/three-hundred-and-sixty";
+import { ThreehundredAndSixtyViewIcon } from "./components/three-hundred-and-sixty-view";
 import { TwelveHoursIcon } from "./components/twelve-hours";
-<<<<<<< HEAD
 import { TwentyFourHoursIcon } from "./components/twenty-four-hours";
 import { TwofaIcon } from "./components/twofa";
-=======
 import { UserCircleIcon } from "./components/user-circle";
 import { ZoomScanIcon } from "./components/zoom-scan";
 import { ZzzIcon } from "./components/zzz";
 import { ZzzOffIcon } from "./components/zzz-off";
->>>>>>> 1e66107dc7ba306a3b7113829e51b4d3a8579be6
 
 export default () => {
   return (
@@ -32,10 +42,8 @@ export default () => {
       <body>
         <TwelveHoursIcon />
         <OneTwoThreeIcon />
-<<<<<<< HEAD
         <TwentyFourHoursIcon />
         <TwofaIcon />
-=======
         <ActivityIcon />
         <BoneIcon />
         <CatIcon />
@@ -52,7 +60,19 @@ export default () => {
         <ZoomScanIcon />
         <ZzzIcon />
         <ZzzOffIcon />
->>>>>>> 1e66107dc7ba306a3b7113829e51b4d3a8579be6
+        <ThreehundredAndSixtyViewIcon />
+        <ThreeHundredAndSixtyIcon />
+        <ThreeDCubesPhereOffIcon />
+        <ThreeDCubesPhereIcon />
+        <ThreeDRotateIcon />
+        <ABTwoIcon />
+        <ABOffIcon />
+        <ABIcon />
+        <AbacusOffIcon />
+        <AbacusIcon />
+        <AbcIcon />
+        <AccessPointOffIcon />
+        <AccessPointIcon />
       </body>
     </>
   );
