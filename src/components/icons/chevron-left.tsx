5dadfc87c1571d02226a7668678082576fbaedfd
edd.ramirez@proton.me <edd.ramirez@proton.me>
2024-03-24 10:21:37 -0500

    Add chevron-left and chevron-up icons

---
+++ src/components/icons/chevron-left.tsx
@@ -0,0 +1,8 @@
+export const IconChevronLeft = () => {
+  return (
+    <>
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M15 6l-6 6l6 6" />
+    </>
+  );
+};
---
+++ src/components/icons/chevron-up.tsx
@@ -0,0 +1,8 @@
+export const IconChevronUp = () => {
+  return (
+    <>
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M6 15l6 -6l6 6" />
+    </>
+  );
+};
