e9c6ed174316a6b9cf294080a6c379b3540b1740
edd.ramirez@proton.me <edd.ramirez@proton.me>
2024-03-24 10:08:23 -0500

    [WIP] icons from u to v

---
+++ src/components/icons/usb.tsx
@@ -0,0 +1,9 @@
+export const IconUsb = () => {
+  return (
+    <>
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M4 12h6l-6 8h6" />
+      <path d="M14 4h6l-6 8h6" />
+    </>
+  );
+};
